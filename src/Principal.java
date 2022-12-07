3cbac008024f351b6ab553fc4d341ea16c140626
andersonvieirarj <andersonvieirarj@gmail.com>
2022-12-07 14:07:14 -0300

    Primeira versão do sistema.

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,17 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Principal {
 
     public static void main(String[] args) {
+        String letra= "#";
+        Scanner ler = new Scanner (System.in);
+        System.out.println("Qual o tamanho da escadinha?");
 
+        int tamanho = ler.nextInt();
+        for (int i=1; i<=tamanho; i++){
+            System.out.println(letra);
+            letra = letra + "#";
+        }
     }
 }
